04a48348fc1f462ec086464750d60cebe54e19e9
DanielAbergel <danielabergel1@gmail.com>
2018-11-13 12:52:14 +0200

    adding to constructor test

--- src/myMath/PolynomTest.java
+++ src/myMath/PolynomTest.java
@@ -20,55 +20,58 @@ class PolynomTest {
 	@Test
 	void testPolynomPolynom() {
 		Polynom test = new Polynom() ; 
-		//
 		excepted_Polynom.add(new Monom(3,2));
 		test.add(new Monom(3,2));
 		actual_Polynom = new Polynom(test);
 		boolean ans = actual_Polynom.equals(excepted_Polynom) ;
-		
-		
-		
+		if(!ans) fail("Not yet implemented");
+		test.add(new Monom(3,2));
+		actual_Polynom = new Polynom(test);
+		ans = actual_Polynom.equals(excepted_Polynom) ;
+		if(ans) fail("Not yet implemented");
+
+
 		fail("Not yet implemented");
 	}
 
 	@Test
 	void testPolynom() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testPolynomString() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testF() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testAddPolynom_able() {
-		fail("Not yet implemented");
+	//	fail("Not yet implemented");
 	}
 
 	@Test
 	void testAddMonom() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testSubstract() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testMultiply() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testEqualsPolynom_able() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
@@ -84,37 +87,37 @@ class PolynomTest {
 
 	@Test
 	void testRoot() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testCopy() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testDerivative() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testArea() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testZero_union() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testIteretor() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 	@Test
 	void testToString() {
-		fail("Not yet implemented");
+//		fail("Not yet implemented");
 	}
 
 }
