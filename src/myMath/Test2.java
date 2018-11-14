9d04e7ecf76a831ee0ac506c0cde0ee6ab35a015
netanelbenis <netnel.b.i@gmail.com>
2018-11-14 11:21:01 +0200

    add a grald folder with the library inside

--- src/myMath/Test2.java
+++ src/myMath/Test2.java
@@ -7,9 +7,9 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		Polynom aba=new Polynom("0.2*x^4 - 1.5*x^3 + 3.0*x^2 - x - 5"); 
 		Polynom ama=new Polynom("5*x^2 - 5");
-//		Graph frame = new Graph(aba,-2.0,6.0);
-//		frame.setVisible(true);
-//		frame.plotting();
+		Graph frame = new Graph(aba,-2.0,6.0);
+		frame.setVisible(true);
+		frame.plotting();
 //		frame.extrema();
 		//		Polynom ama=new Polynom(aba);
 		//		ama.derivative();
